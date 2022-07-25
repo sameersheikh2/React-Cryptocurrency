fdf9db799731babd89fdcf73fc063360cbba98c4
sameersheikh2 <sameersheikh819@gmail.com>
2022-07-25 19:37:32 +0530

    fetch data from coingecko api in app js and passing it into coin.js also removed tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Coins from "./components/Coins";
 
 const App = () => {
-  return (
-    <div>
-    </div>
-  )
-}
+  const [coins, setCoins] = useState([]);
 
-export default App
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=15&page=1&sparkline=false";
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        setCoins(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return <>
+  <Coins coins={coins}/>
+  </>;
+};
+
+export default App;
