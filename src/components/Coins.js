4949b13271b9f13ea013613170e3c4a3acc12c36
sameersheikh2 <sameersheikh819@gmail.com>
2022-07-25 19:38:35 +0530

    passing the data through coin.js to coinitem.js using map

---
+++ src/components/Coins.js
@@ -0,0 +1,22 @@
+import React from "react";
+import CoinItem from "./CoinItem";
+
+const Coins = (props) => {
+  return (
+    <div className="container">
+      <div className="heading">
+        <p>#</p>
+        <p className="coin">Coin</p>
+        <p>Price</p>
+        <p>24h</p>
+        <p className="hide-mobile">Volume</p>
+        <p className="hide-mobile">Mkt Cap</p>
+      </div>
+      {props.coins.map((coins) => {
+        return <CoinItem coins={coins} key={coins.id} />;
+      })}
+    </div>
+  );
+};
+
+export default Coins;
