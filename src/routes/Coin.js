9777781f0219cd494308ec69437515c2ca5fe327
sameersheikh2 <sameersheikh819@gmail.com>
2022-07-29 03:59:10 +0530

    styled coin.js components and show details of coin

--- src/routes/Coin.js
+++ src/routes/Coin.js
@@ -1,30 +1,137 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import axios from "axios";
+import DOMPurify from "dompurify";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-import './Coin.css'
+import "./Coin.css";
 
 const Coin = () => {
-
   const params = useParams();
- const [coin , setCoin] = useState({})
+  const [coin, setCoin] = useState({});
 
- const url = `https://api.coingecko.com/api/v3/coins/${params.coinId}`
+  const url = `https://api.coingecko.com/api/v3/coins/${params.coinId}`;
 
-useEffect(() => {
- axios.get(url).then((response)=>{
-  setCoin(response.data).catch((error)=>{
-    console.log(error)
-  })
- })
-}, [])
-
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        setCoin(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <div>
-        <h1>{coin.id}</h1>
-    </div>
-  )
-}
+      <div className="coin-container">
+        <div className="content">
+          <h1>{coin.name}</h1>
+        </div>
+        <div className="content">
+        <div className="rank">
+          <span className="rank-btn">Rank #{coin.market_cap_rank}</span>
+        </div>
+        <div className="info">
+          <div className="coin-heading">
+            <img src={coin.image?.small} alt="" />
+            <p>{coin.name}</p>
+            <p>{coin.symbol?.toUpperCase()}</p>
+          </div>
+          <div className="coin-price">
+            <h1>{coin.market_data?.current_price.inr}</h1>
+          </div>
+        </div>
+        </div>
+        <div className="content">
+          <table>
+            <thead>
+              <tr>
+                <th>1h</th>
+                <th>24h</th>
+                <th>7d</th>
+                <th>14d</th>
+                <th>30d</th>
+                <th>1yr</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  {coin.market_data?.price_change_percentage_1h_in_currency.inr}
+                </td>
+                <td>
+                  {
+                    coin.market_data?.price_change_percentage_24h_in_currency
+                      .inr
+                  }
+                </td>
+                <td>
+                  {coin.market_data?.price_change_percentage_7d_in_currency.inr}
+                </td>
+                <td>
+                  {
+                    coin.market_data?.price_change_percentage_14d_in_currency
+                      .inr
+                  }
+                </td>
+                <td>
+                  {
+                    coin.market_data?.price_change_percentage_30d_in_currency
+                      .inr
+                  }
+                </td>
+                <td>
+                  {coin.market_data?.price_change_percentage_1y_in_currency.inr}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="content">
+          <div className="stats">
+            <div className="left">
+              <div className="row">
+                <h4>24 Hour Low</h4>
 
-export default Coin
+                <p>{coin.market_data?.low_24h.inr.toLocaleString()}</p>
+              </div>
+              <div className="row">
+                <h4>24 Hour High</h4>
+
+                <p>{coin.market_data?.high_24h.inr.toLocaleString()}</p>
+              </div>
+            </div>
+            <div className="right">
+              <div className="row">
+                <h4>Market Cap</h4>
+
+                <p>{coin.market_data?.market_cap.inr.toLocaleString()}</p>
+              </div>
+              <div className="row">
+                <h4>Circulating Supply</h4>
+
+                <p>{coin.market_data?.circulating_supply}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="content">
+          <div className="about">
+            <h3>About</h3>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(
+                  coin.description ? coin.description.en : ""
+                ),
+              }}
+            ></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Coin;
